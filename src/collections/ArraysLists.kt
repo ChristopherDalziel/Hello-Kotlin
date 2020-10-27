67d869e68ab324ad9cf5485538b1bff782658f93
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2020-10-28 09:18:22 +1100

    updated arrays notes

--- src/collections/ArraysLists.kt
+++ src/collections/ArraysLists.kt
@@ -3,6 +3,18 @@ package collections
 fun main() {
     // Arrays have a fixed length
     val array = arrayOf("QLD", "NSW", "VIC")
+    val newArray = arrayOf("TAS", "WA")
+    // Combine two arrays together
+    val combinedArray = array + newArray
+
+    // Methods
+    println(array.size)
+    val empty: Boolean = array.isEmpty()
+    if (array.contains("NT")) {
+        println("It contains NT")
+    } else {
+        println("It does not contain NT")
+    }
 
     // Lists are immutable
     val list = listOf("Orange", "Apple", "Bananas")
