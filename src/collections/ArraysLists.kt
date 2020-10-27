61f61a2b227574601278c9f7426a7f3ab80257c2
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2020-10-28 09:02:37 +1100

    arrays and lists

---
+++ src/collections/ArraysLists.kt
@@ -0,0 +1,11 @@
+package collections
+
+fun main() {
+    // Arrays have a fixed length
+    val array = arrayOf("QLD", "NSW", "VIC")
+
+    // Lists are immutable
+    val list = listOf("Orange", "Apple", "Bananas")
+
+    val arrayList = arrayListOf("Patrick", "Michael", "Sarah")
+}
