570180821979c606e7eb354b1f8601985b4383d3
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2020-10-28 16:25:59 +1100

    arrays and lists completed

--- src/collections/ArraysLists.kt
+++ src/collections/ArraysLists.kt
@@ -20,4 +20,21 @@ fun main() {
     val list = listOf("Orange", "Apple", "Bananas")
 
     val arrayList = arrayListOf("Patrick", "Michael", "Sarah")
+    // Appends a new value to the end of our arrayList
+    arrayList.add("Steve")
+    // Adds a new value at the selected position within our arrayyList
+    arrayList.add(1, "Bob")
+
+    println(arrayList)
+
+    val removed = arrayList.remove("Michael")
+    // Michael is no longer displayed within our arrayList
+    println(arrayList)
+    // Returns true because Michael was removed
+    println(removed)
+
+    // Specifying where our subList will begin and end, it is not inclusive.. that means the item at position [3] will not be included in our subList
+    // A good way to know how many items will return in your subList is 3 - 1 = 2, so 2 items will be in our new array.
+    val subListArray = arrayList.subList(1, 3)
+    println(subListArray)
 }
