d465575ba0b34cd9ab91cf6c2d1b891ca23baff0
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2020-10-31 09:17:35 +1100

    for loops

--- src/loops/For.kt
+++ src/loops/For.kt
@@ -1,5 +1,25 @@
 package loops
 
 fun main() {
+    // I will become our iterator increasing each loop
+    for (i in 1..10) {
+        print(i)
+    }
 
+    var sum = 0
+    for (i in 1..10) {
+        sum = sum + 1
+        println(sum)
+    }
+
+    val list = listOf("Java", "Kotlin", "Ruby", "JavaScript")
+    // The element variable we use within our for loop will be assigned each value in the array as we loop
+    for (element in list) {
+        println(element)
+    }
+
+    // Here we can loop and return both the index and the value
+    for ((index, value) in list.withIndex()) {
+        println("Element at $index is $value")
+    }
 }
