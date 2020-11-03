82298a1bda499cb5e8e9bd976f6e81701637a8c7
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2020-11-04 09:16:54 +1100

    loops challenge

--- src/loops/LoopsChallenge.kt
+++ src/loops/LoopsChallenge.kt
@@ -1,2 +1,12 @@
 package loops
 
+// Write a program that calculates the sum of all numbers from 100 - 100,000 and prints the result
+// Don't forget about TYPES.. I initially got the wrong result because I returned an INT instead of a LONG
+
+fun main() {
+    var sum = 0L
+    for(i in 100..100000L) {
+        sum += i
+    }
+    println(sum)
+}
