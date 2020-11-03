ef9f5234dcef3aba8e4e7f53203b966a1a50313c
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2020-11-04 08:49:15 +1100

    naming loops

--- src/loops/NamingLoops.kt
+++ src/loops/NamingLoops.kt
@@ -1,2 +1,17 @@
 package loops
 
+fun main() {
+
+    // How we can give our loop a name
+
+    // The @symbol creates the name for our loop and then the break is assigned to this outer loop within our if statement
+    outer@ for (i in 1..10) {
+        for (j in 1..10) {
+            if(i - j == 5) {
+                break@outer
+            }
+            println("$i - $j")
+        }
+    }
+
+}
