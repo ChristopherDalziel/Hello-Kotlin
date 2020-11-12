f0636d7c9dbd06ee0efd6f5643b60b81bf58e3f8
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2020-11-12 16:58:12 +1100

    object example, singleton note

---
+++ src/oO2/ObjectsSingletons.kt
@@ -0,0 +1,12 @@
+package oO2
+
+// A singleton is a class of which there can only ever be one object be defined from it
+
+// An object can only have a single instance - This is called an object declaration
+object Cache {
+    val name = "HyperCache"
+
+    fun retrieveData(): Int {
+        return 0
+    }
+}
